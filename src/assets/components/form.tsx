be75f4147d38eb464fc68c10fe887a7258fcaff6
Joshua Bayron <bayronjoshua333@gmail.com>
2024-05-25 21:07:56 +0800

    make label floating like spirit

--- src/assets/components/form.tsx
+++ src/assets/components/form.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
 
 interface FormNames {
   labelName: string;
@@ -8,9 +9,14 @@ interface FormNames {
 
 const FormComponent: React.FC<FormNames> = ({ labelName, typeName }) => {
   return (
-    <Form.Group className="mb-3">
-      <Form.Label>{labelName}</Form.Label>
-      <Form.Control type={typeName} />
+    <Form.Group className="mb-3"  style={{ maxWidth: '300px' }}>
+      <FloatingLabel
+        controlId="floatingInput"
+        label={labelName}
+        className="mb-3"
+      >
+        <Form.Control type={typeName} placeholder={labelName} />
+      </FloatingLabel>
     </Form.Group>
   );
 };
